6fb96dba0cc405960c05fb655a54f50e1dcf07fc
ZL <ray040406@gmail.com>
2024-10-02 22:51:36 +1300

    dynamic page

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+export default function ProductList() {
+  return (
+    <div>
+      <h1>products</h1>
+      <h1>product 1</h1>
+      <h1>product 2</h1>
+      <h1>product 3</h1>
+    </div>
+  );
+}
