e6cde848d4b0960186bdfbcc04860bbb65a438d5
ZL <ray040406@gmail.com>
2024-10-02 22:57:21 +1300

    type

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,7 @@
-export default function ProductDetails({ params }) {
+export default function ProductDetails({
+  params,
+}: {
+  params: { productId: string };
+}) {
   return <h1>Details about the product {params.productId}</h1>;
 }
