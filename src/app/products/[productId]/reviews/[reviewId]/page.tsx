35334b34f0a603fb6fef883e6017a9ea2dcad2e2
ZL <ray040406@gmail.com>
2024-10-17 23:56:59 +1300

    not found for review

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 export default function ReviewDetail({
   params,
 }: {
@@ -6,6 +7,9 @@ export default function ReviewDetail({
     reviewId: string;
   };
 }) {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
   return (
     <h1>
       Review {params.reviewId} for product {params.productId}
